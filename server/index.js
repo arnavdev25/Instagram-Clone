0bf2b4249141a50bd65f7f2923788683928bb50b
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-07-31 18:51:32 +0530

    socket.io backend setup
    socket.io backend setup

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ require('dotenv').config()
 // console.log(process.env)
 const cors=require("cors")
 const app=express()
+
+// socket.io
+const socket = require("socket.io");
+
+
+
+
+
 app.use(cors())
 app.use(express.json())
 app.use("/auth",authRouter)
@@ -19,3 +27,29 @@ app.listen(process.env.PORT,async()=>{
     await connection;
     console.log(`Server started on ${process.env.PORT}`)
 })
+
+
+
+// socket.io
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
